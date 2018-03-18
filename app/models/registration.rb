a3d844b18fd840e8012748986bfea9e0248c3a06
Astrid Varga <astrid.varga@gmx.de>
2018-03-18 23:07:33 +0100

    Add validation

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,6 +1,20 @@
 require 'travis/encrypt'
 
 class Registration < ApplicationRecord
+  # sets the user’s email address to lower-case, as some database adapters use case-sensitive indices
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: { maximum: 50 }
+  # regex = regular expression, language for matching patterns in strings
+  # used to match valid email addresses while not matching invalid ones
+  # TODO FIX: the following regex allows invalid addresses that contain consecutive dots, such as foo@bar..com.
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  #  Active Record uniqueness validation does not guarantee uniqueness at the database level!
+  # How important is uniqueness of E-Mail?
+  validates :phonenumber, presence: true, length: { maximum: 20 }
+
   def to_csv
     CSV.open("#{Rails.root}/public/registration_#{self.created_at.strftime('%Y-%m-%dT%H%M%S')}.csv", "wb") do |csv|
       csv << self.attributes.keys
