f6c24945b7f677f10f6537886bd96f4079ac0b0e
Astrid Varga <astrid.varga@gmx.de>
2018-03-26 22:19:09 +0200

    clean up stale comments

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,4 @@
 class RegistrationsController < ApplicationController
-  
   def new
     @registration = Registration.new
   end
@@ -8,24 +7,15 @@ class RegistrationsController < ApplicationController
     @registration = Registration.new(registration_params)
     if @registration.save
       render html: "Done."
-      # flash[:success] = "Yay! You're registered! See you at the festival <3"
-      # => This didn't work like that. Maybe another html.erb needed for success message with flash
+      # TODO - create 'registration-sucess' view - stating to check mail and confirm registration 
     else 
       render 'new'
     end
-
-    # if @registration.valid?
-    #   @registration.save
-    #   render html: "Done."
-    # else
-    #   render html: "Nope"
-    # end
   end
   
   private
+
     def registration_params
       params.require(:registration).permit(:name, :email, :phonenumber, :city, :is_member, :contact_person)
     end
-
 end
-
