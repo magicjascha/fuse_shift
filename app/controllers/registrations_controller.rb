d8c327e7b9781c328076e7dc0b93e4702e41d3c0
magicjascha <magicjascha@riseup.net>
2018-04-24 19:28:15 +0200

    finish moving encryption from model to controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,10 +7,10 @@ class RegistrationsController < ApplicationController
     @registration = Registration.new
   end
   
-  def create
-    @registration = Registration.new(registration_params)
+  def create #add hashedEmail, validate that and the raw input data, save encrypted data without validation
+    @registration = Registration.new(add_hashed_email(registration_params))
     if @registration.valid?
-      Registration.new(encrypted_params).save(validate: false)
+      Registration.new(add_hashed_email(params_encrypt(registration_params))).save(validate: false)
       @data = registration_params #contains params that should be displayed in success
       render "success"    
     else 
@@ -18,37 +18,44 @@ class RegistrationsController < ApplicationController
     end
   end
   
-  def edit
+  def edit #identify record by hashedEmail, display an otherwise empty record in the view.
     id = Registration.find_by(hashedEmail: params[:hashed_email]).id
     @registration = Registration.new(id: id, hashedEmail: params[:hashed_email])
-    ##don't display the encrypted stuff in the edit-view or delete @registration and update/save only when field is not empty.
   end
-  
-  def update
-    registration = Registration.find_by(hashedEmail: params[:hashed_email])
-    
+
+  def update #validate raw input data, identify record by hashedEmail, and save encrypted input without validation
     present_attributes = registration_params.select{|key,value| value.present?}
-    @registration.update_attributes(present_attributes)
-    @registration.assign_attributes(registration_params)
-    p @registration
-    p registration_params 
-    render 'edit'
+    @registration = Registration.new(present_attributes)
+    if @registration.valid?
+      @registration = Registration.find_by(hashedEmail: params[:hashed_email])
+      @registration.assign_attributes(params_encrypt(present_attributes))
+      @registration.save(validate: false)
+      render html:'successful update' #build update_success-view and render it here
+    else 
+      render 'edit'
+    end
   end
   
   private
     def registration_params
-      params.require(:registration).permit(:name, :email, :phonenumber, :city, :is_member, :contact_person)
+      r = params.require(:registration).permit(:name, :email, :phonenumber, :city, :is_member, :contact_person)
+      r[:email] = r[:email].downcase if r[:email]
+      r
     end
     
-    def encrypted_params
-      registration_params.to_h.map do |key, value|
+    def add_hashed_email(hash)
+      hash.merge!(hashedEmail: digest(params[:registration][:email]))
+    end
+    
+    def params_encrypt(params)
+      params.to_h.map do |key, value|
         value = encrypt(value) if encrypt?(key)
         [key, value]
-      end.to_h
+      end.to_h                     
     end
     
     def encrypt?(key)
-      ![:is_member, :hashedEmail].include?(key)
+      !["is_member", "hashedEmail"].include?(key)
     end
     
     def encrypt(value)
--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,44 +1,19 @@
 #todo: 
-# validations: phone-number digits and dashes.
+# validations: 
+# phone-number digits and dashes.
+# is_member, city
 # add uniqueness on database level for hashed email
 
 require 'encrypt/encryptor'
 require 'digest'
 
 class Registration < ApplicationRecord
-  include Hasher  
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  
-  before_save on: :create do
-    self.email = email.downcase
-    self.hashedEmail = digest(email)
-    p changes
-  end
-  
+ 
+  #for update only the attributes where the user puts input will be validated.
   validates :hashedEmail, uniqueness: { case_sensitive: false }, if: proc { |r| r.hashedEmail_changed? }
   validates :name, presence: true, length: { maximum: 50 }, if: proc { |r| r.name_changed? }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, if: proc { |r| r.email_changed? }
   validates :phonenumber, presence: true, length: { maximum: 20 }, if: proc { |r| r.phonenumber_changed? }
-  
-  
-#   before_save do
-#     attributes = self.attribute_names - ["id", "created_at", "updated_at", "is_member", "hashedEmail"]
-#     attributes.each do |attribute|
-#       value = self[attribute]
-#       key = File.read('config/keys/public.dev.pem')
-# #       key = "c35237aa3793ac9c22c60eb32291291f8da40b02fae9f0519085a33765f23729cb00be6804097724ddc0a7b30e924cf0bed51c35b1a306857519294cd2ab053b"
-#       value = Encrypt::Encryptor.new(value, key).apply
-#       self[attribute] = value 
-#     end
-#   end
-  
 
-
-  # def name=(name)
-  #   puts "name is: #{name} in Registration"
-  #   key = "c35237aa3793ac9c22c60eb32291291f8da40b02fae9f0519085a33765f23729cb00be6804097724ddc0a7b30e924cf0bed51c35b1a306857519294cd2ab053b"
-  #   name = Travis::Encrypt::Encryptor.new(name, key: key).apply
-  #   puts name
-  #   super(name)
-  # end
 end
