773ad068bd798337fabee671451695a8e3d8fa20
magicjascha <magicjascha@riseup.net>
2018-07-11 21:05:08 +0200

    move better readable registration-value-formats to helper

--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -15,5 +15,17 @@ module RegistrationsHelper
     path = registration_path(registration)
     "#{root_url}#{path[1,path.length-1]}/confirm"
   end
-
+  
+  #change formats of registration values in mails and views
+  def better_read(value)
+    if value.instance_of?(DateTime)
+      l(value, format: :short_datetime)
+    elsif value == "1" 
+      "Yes"
+    elsif value == "0"
+      "No"
+    else
+      value
+    end
+  end 
 end
