3559960183a5170c87d83c2cb6dfac0a431dd51d
co <co@gsmk.de>
2019-02-23 18:19:19 +0100

    db: next migration fix

--- db/migrate/20180815001208_change_id_columnname_in_registrations.rb
+++ db/migrate/20180815001208_change_id_columnname_in_registrations.rb
@@ -1,5 +1,5 @@
 class ChangeIdColumnnameInRegistrations < ActiveRecord::Migration[5.1]
   def change
-    rename_column :registrations, :contact_person_id_id, :contact_person_id
+    # rename_column :registrations, :contact_person_id_id, :contact_person_id
   end
 end
