7f88d7c707d4e4260aa3e9fd02de1f774e60f99a
co <co@gsmk.de>
2019-02-23 18:15:12 +0100

    db: another try to fix migration

--- db/migrate/20180814223712_add_contact_person_id_to_registrations.rb
+++ db/migrate/20180814223712_add_contact_person_id_to_registrations.rb
@@ -1,5 +1,5 @@
 class AddContactPersonIdToRegistrations < ActiveRecord::Migration[5.1]
   def change
-    add_reference :registrations, :contact_person_id, foreign_key: true
+    add_reference :registrations, :contact_person, foreign_key: true
   end
 end
