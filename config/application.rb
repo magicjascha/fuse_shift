b069fe41bf50c152cff21f33c9ad2576b5ebe4f1
co <co@gsmk.de>
2019-02-25 17:33:12 +0100

    app: no need for CUSTOMLOCALE ENV anymore - uses normal file instead

--- config/application.rb
+++ config/application.rb
@@ -18,8 +18,10 @@ module FuseShift
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    File.open("config/locales/en_customize.yml", "w+") do |f|
-        f.write(ENV['CUSTOMLOCALE'])
+    if ENV.has_key?('CUSTOMLOCALE')
+        File.open("config/locales/en_customize.yml", "w+") do |f|
+            f.write(ENV['CUSTOMLOCALE'])
+        end
     end
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
   end
