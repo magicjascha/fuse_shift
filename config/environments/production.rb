309b2bd8a1f8477c61290a1b8da043a0044b294c
co <co@gsmk.de>
2019-02-25 13:39:55 +0100

    prod: fix typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -108,7 +108,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = eval(ENV["MAILERURL"]]
+  config.action_mailer.default_url_options = eval(ENV["MAILERURL"])
   config.action_mailer.smtp_settings = {
     address:              'elektropost.org',
     port:                 587,
