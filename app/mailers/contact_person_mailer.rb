3e91e9021e8d6b03b7732bfd14be38ad43b9ca69
co <co@gsmk.de>
2019-03-05 12:00:38 +0100

    mailer: mail from from ENV

--- app/mailers/contact_person_mailer.rb
+++ app/mailers/contact_person_mailer.rb
@@ -1,6 +1,6 @@
 class ContactPersonMailer < ApplicationMailer
-  default from: "no-reply@festival-registration.de"
-  
+  default from: ENV["MAILUSER"]
+
   def confirm(contact_person, contact_persons_email)
     @contact_person = contact_person
     mail(to: contact_persons_email, subject: I18n.t("mail.contact_person_confirm.subject"))
--- test/mailers/contact_person_mailer_test.rb
+++ test/mailers/contact_person_mailer_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ContactPersonMailerTest < ActionMailer::TestCase
   include Rails.application.routes.url_helpers
-  
+
   test "confirm email" do
     #setup email and @contact_person
     contact_persons_email = "sandra@mail.de"
@@ -10,11 +10,12 @@ class ContactPersonMailerTest < ActionMailer::TestCase
     #check email from mailer
     email = ContactPersonMailer.confirm(@contact_person, contact_persons_email)
     #check email addresses and suject
-    assert_equal ["no-reply@festival-registration.de"], email.from
+    #assert_equal ["no-reply@festival-registration.de"], email.from
+    assert_equal [ENV["MAILUSER"]], email.from
     assert_equal [contact_persons_email], email.to
     assert_equal I18n.t("mail.contact_person_confirm.subject"), email.subject
     #check email-body for confirm-link
     assert_match("#{root_url}contact_persons/#{@contact_person.hashed_email}/warning", email.html_part.body.decoded)
   end
-  
+
 end
