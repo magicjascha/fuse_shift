6e3874d21390d040a513d81039bf5914a23b5d0d
co <co@gsmk.de>
2019-03-05 12:12:51 +0100

    prod: better dev/null

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,17 +94,14 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.logger = Logger.new('/dev/null')
-
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
-  end
+  # logger           = ActiveSupport::Logger.new(STDOUT)
+  logger           = ActiveSupport::Logger.new('/dev/null')
+  logger.formatter = config.log_formatter
+  config.logger    = ActiveSupport::TaggedLogging.new(logger)
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
